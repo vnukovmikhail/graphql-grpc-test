cccb6efb98f0bc909ba354d46ee0f965687395f1
Mihail <vnukovmisa72@gmail.com>
2026-08-29 21:53:45 +0300

    First gRPC service

---
+++ tiago/services/orders/grpc.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"log"
+	"net"
+
+	handler "GRPC/tiago/services/orders/handler/orders"
+	"GRPC/tiago/services/orders/service"
+
+	"google.golang.org/grpc"
+)
+
+type gRPCServer struct {
+	addr string
+}
+
+func NewGRPCServer(addr string) *gRPCServer {
+	return &gRPCServer{addr: addr}
+}
+
+func (s *gRPCServer) Run() error {
+	lis, err := net.Listen("tcp", s.addr)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	grpcServer := grpc.NewServer()
+	ordersService := service.NewOrderService()
+	handler.NewGrpcOrdersService(grpcServer, ordersService)
+
+	log.Println("Starting gRPC server on", s.addr)
+
+	return grpcServer.Serve(lis)
+}
---
+++ tiago/services/orders/types/types.go
@@ -0,0 +1,12 @@
+package types
+
+import (
+	"context"
+
+	"GRPC/tiago/services/common/genproto/orders"
+)
+
+type OrderService interface {
+	CreateOrder(context.Context, *orders.Order) error
+	GetOrders(context.Context) []*orders.Order
+}
